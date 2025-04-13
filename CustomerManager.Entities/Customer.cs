430e0bef0f2cc8b734072c9a3e16c4c5841a2a53
Argenis Villarroel <smitterargenis@outlook.com>
2025-04-13 11:46:51 -0400

    Simple Crud sin base de datos

---
+++ CustomerManager.Entities/Customer.cs
@@ -0,0 +1,69 @@
+﻿namespace CustomerManager.Entities;
+
+public class Customer
+{
+    public static Customer Instance(int id, string firstName, string lastName, string type, string address, string user) => new(id, firstName, lastName, type, address, user);
+
+    private Customer(int id, string firstName, string lastName, string type, string address, string user)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Type = type;
+        Address = address;
+        CreatedBy = user;
+        CreatedOn = DateTime.Now;
+        LastModifiedBy = null;
+        LastModifiedOn = null;
+        DeletedBy = null;
+        DeletedOn = null;
+        IsDelete = false;
+    }
+
+    public int Id { get; private set; } 
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Type { get; private set; }
+    public string Address { get; private set; }
+
+    public string FullName => $"{FirstName}  {LastName}";
+    public string AuditInfo => $"Creado: {CreatedBy}/{CreatedOn} -  Modificado {LastModifiedBy}/{LastModifiedOn} - Eliminado {IsDelete} {DeletedBy}/{DeletedOn}";
+
+    public string CreatedBy { get; private set; }
+    public DateTime CreatedOn { get; private set; }
+    public string? LastModifiedBy { get; private set; }
+    public  DateTime? LastModifiedOn { get; private set; }
+
+    public string? DeletedBy {  get; private set; }
+    public DateTime? DeletedOn { get; private set; }
+    public bool IsDelete { get; private set; }
+
+    public void ChangeName(string fistName, string lastName, string user)
+    {
+        FirstName = fistName;
+        LastName = lastName;
+        LastModifiedEntry(user, DateTime.Now);
+    }
+
+    public void LastModifiedEntry(string lastModifiedBy, DateTime lastModifiedOn)
+    {
+        LastModifiedBy = lastModifiedBy;
+        LastModifiedOn = lastModifiedOn;
+    }
+
+    public void Delete(string user)
+    {
+        if (IsDelete)
+        {
+            IsDelete = false;
+            DeletedBy = null;
+            DeletedOn = null;
+        }
+        else
+        {
+            IsDelete = true;
+            DeletedBy = user;
+            DeletedOn = DateTime.Now;
+        }
+    }
+}
--- CustomerManager.UI/Program.cs
+++ CustomerManager.UI/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Bienvenido a Administrador de Clientes");
+
+Customer customerTest = Customer.Instance(1, "Argenis", "Villarroel", "Desconocida", "Contado", Environment.UserName);
+
+Console.WriteLine(customerTest.FullName);
+Console.WriteLine(customerTest.AuditInfo);
+
+customerTest.ChangeName("Arturo", "Davila", Environment.UserName);
+
+Console.WriteLine(customerTest.FullName);
+Console.WriteLine(customerTest.AuditInfo);
+
+customerTest.Delete(Environment.UserName);
+
+Console.WriteLine(customerTest.FullName);
+Console.WriteLine(customerTest.AuditInfo);
